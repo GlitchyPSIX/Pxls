4bdb9a54f494f16d86488d308dd7bd2af16b4b5f
GlitchyPSIX <supermglitchy3@gmail.com>
2024-07-03 21:58:23 -0400

    add extra check for server membership to Discord auth

--- src/main/java/space/pxls/auth/DiscordAuthService.java
+++ src/main/java/space/pxls/auth/DiscordAuthService.java
@@ -7,7 +7,10 @@ import kong.unirest.UnirestException;
 import kong.unirest.json.JSONObject;
 import space.pxls.App;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.StreamSupport;
 
 public class DiscordAuthService extends AuthService {
     public DiscordAuthService(String id) {
@@ -15,7 +18,9 @@ public class DiscordAuthService extends AuthService {
     }
 
     public String getRedirectUrl(String state) {
-        return "https://discord.com/api/oauth2/authorize?client_id=" + App.getConfig().getString("oauth.discord.key") + "&response_type=code&redirect_uri=" + getCallbackUrl() + "&duration=temporary&scope=identify&state=" + state;
+        boolean requiresMembership = App.getConfig().getBoolean("oauth.discord.requireMembership");
+        String oauthScopes = requiresMembership ? "identify+guilds" : "identify";
+        return "https://discord.com/api/oauth2/authorize?client_id=" + App.getConfig().getString("oauth.discord.key") + "&response_type=code&redirect_uri=" + getCallbackUrl() + "&duration=temporary&scope="+oauthScopes+"&state=" + state;
     }
 
     public String getToken(String code) throws UnirestException {
@@ -41,21 +46,50 @@ public class DiscordAuthService extends AuthService {
                 .header("Authorization", "Bearer " + token)
                 .header("User-Agent", "pxls.space")
                 .asJson();
-        JSONObject json = me.getBody().getObject();
-        if (json.has("error")) {
-            return null;
-        } else {
-            long id = json.getLong("id");
-            long signupTimeMillis = (id >> 22) + 1420070400000L;
-            long ageMillis = System.currentTimeMillis() - signupTimeMillis;
 
-            long minAgeMillis = App.getConfig().getDuration("oauth.discord.minAge", TimeUnit.MILLISECONDS);
-            if (ageMillis < minAgeMillis){
-                long days = minAgeMillis / 86400 / 1000;
-                throw new InvalidAccountException("Account too young");
-            }
-            return json.getString("id");
+
+        JSONObject json = me.getBody().getObject();
+
+        boolean requiresMembership = App.getConfig().getBoolean("oauth.discord.requireMembership");
+
+        if (json.has("error")) return null;
+
+        long id = json.getLong("id");
+        long signupTimeMillis = (id >> 22) + 1420070400000L;
+        long ageMillis = System.currentTimeMillis() - signupTimeMillis;
+
+        long minAgeMillis = App.getConfig().getDuration("oauth.discord.minAge", TimeUnit.MILLISECONDS);
+        if (ageMillis < minAgeMillis){
+            long days = minAgeMillis / 86400 / 1000;
+            throw new InvalidAccountException("Account too young");
         }
+
+        if (requiresMembership){
+            HttpResponse<JsonNode> guilds = Unirest.get("https://discord.com/api/users/@me/guilds")
+                    .header("Authorization", "Bearer " + token)
+                    .header("User-Agent", "pxls.space")
+                    .asJson();
+
+            long requiredServerId = App.getConfig().getLong("oauth.discord.membershipServerId");
+
+            // time for jank: why does the stream api not have a straightforward way to
+            // turn an iterator into a Spliterator?
+
+            Spliterator<Object> guildsSplit = Spliterators.spliteratorUnknownSize(guilds.getBody().getArray().iterator(),
+                    Spliterator.ORDERED);
+
+            boolean isInServer =
+                    StreamSupport.stream(guildsSplit, false)
+                    .anyMatch(server -> ((JSONObject) server).getLong("id") == requiredServerId);
+
+            if (!isInServer){
+                throw new InvalidAccountException("Account is not in the required Discord server");
+            }
+
+        }
+
+        return json.getString("id");
+
     }
 
     public String getName() {
